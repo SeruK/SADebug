41345a82e5f17bf0a41b4a6ce44f1ccf47fab10b
SeruK <strandsebastian2000@gmail.com>
2016-02-23 00:14:01 +0100

    * Made a more proper sample Unity project

---
+++ Examples/ExampleProject/Assets/Scripts/DebugTest.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UE = UnityEngine;
+using System.Collections;
+
+public class DebugTest : MonoBehaviour, IDebugSquelcher {
+	#region Fields
+	private static readonly DebugContext STATIC_CONTEXT = Dbg.Context( "I'm not an Unity object" );
+	
+	private DefaultDebugSystem debugSystem;
+	#endregion
+
+	#region Mono
+	private void OnEnable() {
+		debugSystem = new DefaultDebugSystem();
+		Dbg.DebugSystem = debugSystem;
+
+		// Log
+		Dbg.Log( STATIC_CONTEXT, "Log {0}", "hello" );
+		Dbg.Log( "Dbg.Log(msg)" );
+		Dbg.Log( this, "Dbg.Log(ctx, msg)" );
+		Dbg.LogIf( true, "Dbg.LogIf(cond, msg)" );
+		Dbg.LogIf( true, this, "Dbg.LogIf(cond, ctx, msg)" );
+
+		Dbg.LogRelease( "Dbg.LogRelease(msg)" );
+		Dbg.LogRelease( this, "Dbg.LogRelease(ctx, msg)" );
+		Dbg.LogReleaseIf( true, "Dbg.LogReleaseIf(cond, msg)" );
+		Dbg.LogReleaseIf( true, this, "Dbg.LogReleaseIf(cond, ctx, msg)" );
+
+		// LogWarn
+		Dbg.LogWarn( "Dbg.LogWarn(msg)" );
+		Dbg.LogWarn( this, "Dbg.LogWarn(ctx, msg)" );
+		Dbg.LogWarnIf( true, "Dbg.LogWarnIf(cond, msg)" );
+		Dbg.LogWarnIf( true, this, "Dbg.LogWarnIf(cond, ctx, msg)" );
+
+		Dbg.LogWarnRelease( "Dbg.LogWarnRelease(msg)" );
+		Dbg.LogWarnRelease( this, "Dbg.LogWarnRelease(ctx, msg)" );
+		Dbg.LogWarnReleaseIf( true, "Dbg.LogWarnReleaseIf(cond, msg)" );
+		Dbg.LogWarnReleaseIf( true, this, "Dbg.LogWarnReleaseIf(cond, ctx, msg)" );
+
+		// LogError
+		Dbg.LogError( "Dbg.LogError(msg)" );
+		Dbg.LogError( this, "Dbg.LogError(ctx, msg)" );
+		Dbg.LogErrorIf( true, "Dbg.LogErrorIf(cond, msg)" );
+		Dbg.LogErrorIf( true, this, "Dbg.LogErrorIf(cond, ctx, msg)" );
+
+		Dbg.LogErrorRelease( "Dbg.LogErrorRelease(msg)" );
+		Dbg.LogErrorRelease( this, "Dbg.LogErrorRelease(ctx, msg)" );
+		Dbg.LogErrorReleaseIf( true, "Dbg.LogErrorReleaseIf(cond, msg)" );
+		Dbg.LogErrorReleaseIf( true, this, "Dbg.LogErrorReleaseIf(cond, ctx, msg)" );
+
+		Dbg.Log( this, "Hello" );
+		Dbg.Log( this, "Dbg.Log" );
+		Dbg.LogWarn( this, "Dbg.LogWarn" );
+		Dbg.LogError( this, "Dbg.LogError" );
+		Dbg.Assert.IsTrue( true, this, "true should be true" );
+		try {
+			Dbg.Assert.IsTrue( false, this, "true should not be false" );
+		} catch( System.Exception exc ) {
+			Dbg.LogError( this, "Dbg.LogError in catch" );
+			Dbg.LogExc( this, exc );
+		}
+
+		InvokeRepeating( "Spam", 0.5f, 0.5f );
+		InvokeRepeating( "ImportantMessage", 5.0f, 3.0f );
+	}
+	#endregion
+
+	#region IDebugSystem
+	public bool ShouldSquelchLog( LogType logType, System.Exception exc, string message ) {
+		return logType == LogType.Log;
+	}
+	#endregion
+
+	#region Logging
+	void Spam() {
+		Dbg.Log( this, "Spam" );
+	}
+
+	void ImportantMessage() {
+		Dbg.LogError( this, "Ah! Something bad happened" );
+	}
+	#endregion
+
+	#region GUI
+	void OnGUI() {
+		foreach( var log in debugSystem.Logs ) {
+			UE.Object obj = log.Object;
+			GUILayout.Label( obj == null ? "<null>" : obj.name );
+			foreach( var entry in log.Logs ) {
+				DrawEntry( entry.Frame, entry.LogType, entry.Message );
+			}
+		}
+	}
+
+	void DrawEntry( int frame, LogType logType, string message ) {
+		Color color = logType == LogType.Log ? Color.white :
+			logType == LogType.Warning ? Color.yellow :
+			Color.red;
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Space( 20.0f );
+
+		GUI.color = color;
+		GUILayout.Label( string.Format( "{0}:", frame ), GUILayout.MinWidth( 60.0f ) );
+		GUILayout.Label( message );
+		GUI.color = Color.white;
+
+		GUILayout.EndHorizontal();
+	}
+	#endregion
+}
---
+++ Examples/ExampleProject/Assets/Scripts/DefaultDebugSystem.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UE = UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Tracks UnityEngine.Object contexts and provides a per-object log.
+public class DefaultDebugSystem : IDebugSystem {
+	#region Types
+	public struct LogEntry {
+		public readonly int Frame;
+		public readonly LogType LogType;
+		public readonly string Message;
+
+		public LogEntry( int frame, LogType logType, Exception exc, string message ) {
+			this.Frame = frame;
+			this.LogType = logType;
+			this.Message = message;
+		}
+	}
+
+	public struct ObjectLog {
+		public UE.Object Object {
+			get { return (UE.Object)objectRef.Target; }
+		}
+		public IEnumerable<LogEntry> Logs {
+			get { return logs; }
+		}
+
+		private readonly WeakReference objectRef;
+		private readonly Queue<LogEntry> logs;
+		private readonly int logLength;
+
+		public ObjectLog( UE.Object obj, int logLength ) {
+			this.objectRef = new WeakReference( obj );
+			this.logs = new Queue<LogEntry>();
+			this.logLength = logLength;
+		}
+
+		public void AddLogEntry( LogEntry entry ) {
+			if( logs.Count >= logLength ) {
+				logs.Dequeue();
+			}
+			logs.Enqueue( entry );
+		}
+	}
+	#endregion
+
+	#region Fields
+	private Dictionary<int, ObjectLog> entries;
+	#endregion
+
+	#region Properties
+	public ICollection<ObjectLog> Logs {
+		get { return entries.Values; }
+	}
+	#endregion
+
+	#region Constructors
+	public DefaultDebugSystem() {
+		entries = new Dictionary<int, ObjectLog>();
+	}
+	#endregion
+
+	#region Methods
+	public void AddLogEntry( LogType logType, object ctx, Exception exc, string message, out bool squelch ) {
+		squelch = false;
+		var obj = ctx as UE.Object;
+
+		if( obj == null ) {
+			return;
+		}
+
+		int instanceId = obj.GetInstanceID();
+		if( !entries.ContainsKey( instanceId ) ) {
+			entries[ instanceId ] = new ObjectLog( obj, logLength: 10 );
+		}
+		entries[ instanceId ].AddLogEntry( new LogEntry( Time.frameCount, logType, exc, message ) );
+	}
+
+	public void Clean() {
+		// TODO: This nicer (IndexBuffer)
+		var keys = entries.Keys;
+		
+		bool anyDirty = false;
+		foreach( int key in keys ) {
+			if( entries[ key ].Object == null ) {
+				anyDirty = true;
+				break;
+			}
+		}
+
+		if( anyDirty ) {
+			List<int> copiedKeys = new List<int>( entries.Keys );
+			for( int keyIndex = 0; keyIndex < copiedKeys.Count; ++keyIndex ) {
+				int key = copiedKeys[ keyIndex ];
+				if( entries[ key ].Object == null ) {
+					entries.Remove( key );
+				}
+			}
+		}
+	}
+	#endregion
+}
